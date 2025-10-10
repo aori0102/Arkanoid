2b7d4a1514cf04be15646f6ed7ee46dc1002375b
aori0102 <24021441@vnu.edu.vn>
2025-10-10 13:46:17 +0700

    Rollback merge from Lo2d

--- src/main/java/game/Init.java
+++ src/main/java/game/Init.java
@@ -5,8 +5,7 @@ import org.*;
 public class Init {
 
     public static void Init_Kine() {
-        var bocchiObject = GameObjectManager.instantiate("Bocchi");
-        bocchiObject.addComponent(Test.class);
+
     }
 
     public static void Init_Dui() {
