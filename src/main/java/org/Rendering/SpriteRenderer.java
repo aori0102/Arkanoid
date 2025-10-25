81f98d6addff08d7e9e7b26b780e4e4ba2fe1a58
aori0102 <24021441@vnu.edu.vn>
2025-10-25 22:05:26 +0700

    SpriteRenderer update - Added opacity

--- src/main/java/org/Rendering/SpriteRenderer.java
+++ src/main/java/org/Rendering/SpriteRenderer.java
@@ -120,6 +120,16 @@ public class SpriteRenderer extends Renderable {
         updateRotationPivot();
     }
 
+    /**
+     * Set the rendered image's opacity, with {@code 1} being fully
+     * opaque, {@code 0} being fully transparent.
+     *
+     * @param opacity The image opacity to set.
+     */
+    public void setOpacity(Double opacity) {
+        sprite.setOpacity(opacity);
+    }
+
     private void updateRotationPivot() {
         var globalPivot = getPivotPoint();
         rotateProperty.setPivotX(globalPivot.x);
@@ -355,11 +365,9 @@ public class SpriteRenderer extends Renderable {
 
     }
 
+    @Override
     public Node getNode() {
         return sprite;
     }
 
-    public ImageView getSprite() {
-        return sprite;
-    }
 }
