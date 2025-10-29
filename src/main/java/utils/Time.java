882dd4e9134896a74f5aa2782b4c56e074f46a28
aori0102 <24021441@vnu.edu.vn>
2025-10-29 16:13:58 +0700

    Time update - Changed access modifiers for time utility.

--- src/main/java/utils/Time.java
+++ src/main/java/utils/Time.java
@@ -24,17 +24,9 @@ public class Time {
     private static final HashMap<CoroutineID, Coroutine> coroutineMap = new HashMap<>();
 
     private static double timeScale = 1.0;
-
-    // TODO: private plz
-    /**
-     * The current time in seconds from the start of the application.
-     */
-    public static double time = 0.0;
-
-    /**
-     * The time difference in seconds between this and last frame.
-     */
-    public static double deltaTime = 0.0;
+    private static double time = 0.0;
+    private static double realTime = 0.0;
+    private static double deltaTime = 0.0;
 
     /**
      * Update time for this frame. Should only be
@@ -49,9 +41,10 @@ public class Time {
 
     private static void updateTime() {
 
-        var prev = time;
-        time = System.nanoTime() / 1000000000.0 - offset;
-        deltaTime = (time - prev) * timeScale;
+        var prev = realTime;
+        realTime = System.nanoTime() / 1000000000.0 - offset;
+        deltaTime = (realTime - prev) * timeScale;
+        time += deltaTime;
 
     }
 
@@ -73,13 +66,14 @@ public class Time {
      * after a set amount of time.
      *
      * @param action The action to be executed.
-     * @param tick   The time in seconds after upon
+     * @param tick   The time in seconds upon
      *               when the action will be executed.
      * @return A coroutine ID of the created coroutine. It
      * can be used via {@link #removeCoroutine} to
      * manually terminate the coroutine if needed.
      */
     public static CoroutineID addCoroutine(Runnable action, double tick) {
+        //TODO: should consider using delay rather than certain tick
         var id = new CoroutineID();
         var coroutine = new Coroutine(action, tick);
         coroutineMap.put(id, coroutine);
@@ -104,6 +98,39 @@ public class Time {
      */
     public static void setTimeScale(double timeScale) {
         Time.timeScale = timeScale;
+    }
+
+    /**
+     * Scaling of game time. Determine how fast or slow time progresses.
+     *
+     * @return The scaling of game time.
+     */
+    public static double getTimeScale() {
+        return timeScale;
+    }
+
+    /**
+     * The game's current time in seconds from the start of the application.
+     * This takes {@code timeScale} into consideration, so the game's time will
+     * be slower or faster than {@link #realTime}.
+     */
+    public static double getTime() {
+        return time;
+    }
+
+    /**
+     * The real time in seconds from the start of the application.
+     */
+    public static double getRealTime() {
+        return realTime;
+    }
+
+
+    /**
+     * The time difference in seconds between this and last frame.
+     */
+    public static double getDeltaTime() {
+        return deltaTime;
     }
 
 }
