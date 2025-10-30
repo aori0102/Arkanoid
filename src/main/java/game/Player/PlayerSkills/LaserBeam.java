47066fe12b40e84ba7edb989c6d7364ea4adc95c
aori0102 <24021441@vnu.edu.vn>
2025-10-30 18:26:57 +0700

    Laser update - Modified laser beam to adapt with new damaging system.

--- src/main/java/game/Player/PlayerSkills/LaserBeam.java
+++ src/main/java/game/Player/PlayerSkills/LaserBeam.java
@@ -1,9 +1,13 @@
 package game.Player.PlayerSkills;
 
-import game.Brick.Brick;
+import game.Damagable.DamageAcceptor;
+import game.Damagable.DamageInfo;
+import game.Damagable.DamageType;
+import game.Damagable.ICanDealDamage;
 import game.GameObject.Border.Border;
 import game.GameObject.Border.BorderType;
-import game.Voltraxis.Interface.ITakePlayerDamage;
+import game.Voltraxis.Object.PowerCore.PowerCoreDamageAcceptor;
+import game.Voltraxis.VoltraxisDamageAcceptor;
 import org.GameObject.GameObject;
 import org.GameObject.GameObjectManager;
 import org.Layer.Layer;
@@ -12,7 +16,7 @@ import org.Physics.CollisionData;
 import utils.Time;
 import utils.Vector2;
 
-public class LaserBeam extends Skill{
+public class LaserBeam extends Skill implements ICanDealDamage {
 
     private static final double LASER_SPEED = 1000;
     private static final int LASER_DAMAGE = 36;
@@ -42,13 +46,6 @@ public class LaserBeam extends Skill{
 
     public void handleCollision(CollisionData collisionData) {
 
-        var target = collisionData.otherCollider.getComponent(ITakePlayerDamage.class);
-        if (target != null) {
-            target.takeDamage(LASER_DAMAGE);
-            GameObjectManager.destroy(gameObject);
-            return;
-        }
-
         var border = collisionData.otherCollider.getComponent(Border.class);
         if (border != null && border.getBorderType() == BorderType.BorderTop) {
             GameObjectManager.destroy(gameObject);
@@ -71,5 +68,24 @@ public class LaserBeam extends Skill{
         //TODO: Add event
     }
 
+
+    @Override
+    public DamageInfo getDamageInfo() {
+        var damageInfo = new DamageInfo();
+        damageInfo.amount = LASER_DAMAGE;
+        damageInfo.type = DamageType.Normal;
+        return damageInfo;
+    }
+
+    @Override
+    public void onDamaged() {
+        GameObjectManager.destroy(gameObject);
+    }
+
+    @Override
+    public boolean isDamageTarget(DamageAcceptor damageAcceptor) {
+        return damageAcceptor instanceof PowerCoreDamageAcceptor
+                || damageAcceptor instanceof VoltraxisDamageAcceptor;
+    }
 
 }
